86198b1cc421f0e07bd8f0d8d523d74e76f2dbd6
xiaochuan <Zhao2230638015@163.com>
2022-09-22 15:07:01 +0800

    购物车1.0.0

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="app-container">
+    <!-- 实例化header组件 -->
+    <Header></Header>
+    <!-- 实例化Goods组件 -->
+    <Goods
+     v-for="item in list" 
+     :key="item.id" 
+     :id="item.goods_id"
+     :title="item.goods_name" 
+     :pic="item.goods_img" 
+     :price="item.goods_price"
+     :state="item.goods_state"
+     :count="item.goods_count"
+     @state-change="getNewState"
+     ></Goods>
+
+     <!-- 实例化Footer组件 -->
+     <Footer :checkState="fullChecked"  @state-change="getFooterStateChange" :amount="awt" :allCount="allCount"></Footer>
+
+  </div>
+</template>
+
+<script>
+//导入axios
+import axios from 'axios'
+import Header from "@/components/Header/Header.vue";
+import Goods from '@/components/Goods/Goods.vue'
+import Footer from '@/components/Footer/Footer.vue'
+import bus from '@/components/eventBus.js'
+export default {
+  components: {
+    Header,
+    Goods,
+    Footer
+  },
+  data() {
+    return {
+      list:[],
+    }
+  },
+  methods: {
+    async toGetData(){
+      const {data:res}=await axios.get('https://applet-base-api-t.itheima.net/api/cart')
+      console.log(res);
+      if(res.status===200){
+        this.list=res.list;
+      }
+    },
+    getNewState(val){
+      this.list.some(item=>{
+        if(item.goods_id===val.goods_id){
+          item.goods_state=val.value;
+          return true;
+        }
+      })
+    },
+    getFooterStateChange(val){
+      console.log(val);
+      this.list.forEach(item=>{
+        item.goods_state=val;
+      })
+    }
+  },
+  computed: {
+    fullChecked(){
+      return this.list.every(item=>item.goods_state===true)
+    },
+    awt(){
+      return this.list.filter(item=>item.goods_state===true).reduce((total,item)=>{
+        return total+=item.goods_count*item.goods_price
+      },0)
+    },
+    allCount(){
+      return this.list.filter(item=>item.goods_state).reduce((total,item)=>{
+        return total+=item.goods_count;
+      },0)
+    }
+  },
+  created() {
+    this.toGetData(),
+    bus.$on('count-change',val=>{
+      this.list.some(item=>{
+        if(item.goods_id===val.id){
+          item.goods_count=val.value;
+          return true;
+        }
+      })
+    })
+    
+  },
+
+
+}
+</script>
+
+<style lang="less" scoped>
+.app-container {
+  padding-top: 45px;
+  padding-bottom: 50px;
+}
+</style>
---
+++ src/components/Counter/Counter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="number-container d-flex justify-content-center align-items-center">
+    <!-- 减 1 的按钮 -->
+    <button type="button" class="btn btn-light btn-sm" @click="subtract" ref="subRef">-</button>
+    <!-- 购买的数量 -->
+    <span class="number-box">{{num}}</span>
+    <!-- 加 1 的按钮 -->
+    <button type="button" class="btn btn-light btn-sm" @click="add">+</button>
+  </div>
+</template>
+
+<script>
+  import bus from '@/components/eventBus.js'
+export default {
+  props:{
+    num:{
+      type:Number,
+      default:0
+    },
+    id:{
+      type:Number,
+    }
+  },
+  methods: {
+    add(){
+      this.$refs.subRef.disabled=false;
+      bus.$emit('count-change',{id:this.id,value:this.num+1})
+    },
+    subtract(){
+      if(this.num-1===0) {
+        this.$refs.subRef.disabled=true;
+      }
+      bus.$emit('count-change',{id:this.id,value:this.num-1})
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.number-box {
+  min-width: 30px;
+  text-align: center;
+  margin: 0 5px;
+  font-size: 12px;
+}
+
+.btn-sm {
+  width: 30px;
+}
+</style>
